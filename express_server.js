aa18bce45bf68e8d5e96848faf642a0fdec2e06a
SpinnyFinny <finn.hiltner@hotmail.com>
2017-10-11 14:38:56 -0700

    added form that takes URLs and gives a random string; added redirecting to form post request; added get request that redirects from /u/shortURL to longURL

--- express_server.js
+++ express_server.js
@@ -4,9 +4,23 @@ var PORT = process.env.PORT || 8080;
 
 app.set('view engine','ejs');
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 function routeHandler(req, res){
 
 }
+
+function randomString() {
+    var randomString = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < 6; i++)
+      randomString += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return randomString;
+  }
+
 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,6 +36,31 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
+
+// GET /notes/
+// GET /notes/new
+// POST/PUT /notes/
+// GET /notes/noteId
+// POST/PUT /notes/noteId
+// DELETE /notes/noteId
+
+// BREAD
+
+
+
+//get this esplained
+app.post("/urls", (req, res) => { 
+    let longURL = req.body.longURL
+    let shortURL = randomString();
+    urlDatabase[shortURL] = longURL;
+    //res.send('this will be a redirect to the long url')
+    //TODO redirect
+    res.redirect("urls/" + shortURL); 
+});
+
 app.get("/urls/:id", (req, res) => {
     const shortURL = req.params.id;
     const longURL = urlDatabase[shortURL];
@@ -30,14 +69,20 @@ app.get("/urls/:id", (req, res) => {
     res.render("urls_show", templateVars);
   });
 
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
+  // are these the same???^v
+
+  app.get("/u/:shortURL", (req, res) => {
+    let longURL = urlDatabase[req.params.shortURL]
+    res.redirect(longURL);
   });
 
-app.get("/hello", (req, res) => {
-    res.end("<html><body>Hello <b>World</b></body></html>\n");
+  app.listen(PORT, () => {
+    console.log(`TinyApp server listening on port ${PORT}!`);
 });
 
-app.listen(PORT, () => {
-    console.log(`Serverlistening on port ${PORT}!`);
-});
+
+  // DOn't think I need this
+// app.get("/urls.json", (req, res) => {
+//     res.json(urlDatabase);
+//   });
+
